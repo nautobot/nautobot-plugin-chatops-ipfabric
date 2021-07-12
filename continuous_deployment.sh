b5a854ed40bda1198d16b3d5494087a3382d3aec
Christian Adell <chadell@gmail.com>
2021-07-12 15:37:39 +0200

    Add SSH key as a private secret file

--- continuous_deployment.sh
+++ continuous_deployment.sh
@@ -5,23 +5,16 @@ set -euo pipefail
 # Hardcoded IP for the development server
 TARGET_HOST_IP=159.65.241.133
 
-if [ -z "$TARGET_HOST_KEY" ]
-then
-    echo "TARGET_HOST_KEY has not been provided."
-    exit 1
-else
-    mkdir -p ~/.ssh
-    echo "${TARGET_HOST_KEY}" >> ~/.ssh/ssh_key_ipfabric
-    chmod 600 ~/.ssh/ssh_key_ipfabric
-fi
-
+chmod 600  /tmp/kitt-generic-ssh-key
+eval "$(ssh-agent -s)"
+openssl aes-256-cbc -K $encrypted_7d8846f60ae2_key -iv $encrypted_7d8846f60ae2_iv -in kitt-generic-ssh-key.enc -out /tmp/kitt-generic-ssh-key -d
 echo "INFO: Connecting via SSH to ${TARGET_HOST_IP}"
 
 # There are a lot of assumptions taken to simplify such as:
 # - the folder /opt/chatbot-ipfabric is there and git is already cloned and has a deployment key to interact with git
 # - poetry is installed
 
-ssh -t -o StrictHostKeyChecking=no root@"${TARGET_HOST_IP}" -i  ~/.ssh/ssh_key_ipfabric << EOF
+ssh -t -o StrictHostKeyChecking=no root@"${TARGET_HOST_IP}" -i  /tmp/kitt-generic-ssh-key << EOF
     cd /opt/chatbot-ipfabric
     git checkout develop
     git reset --hard origin/develop
