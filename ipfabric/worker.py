100a79e251dbb1b04c5b45fd11c8315953c5725e
Christian Adell <chadell@gmail.com>
2021-07-13 16:28:57 +0200

    New get_int_load command

--- ipfabric/worker.py
+++ ipfabric/worker.py
@@ -35,21 +35,8 @@ def prompt_hello_input(action_id, help_text, dispatcher, choices=None):
     return False
 
 
-@subcommand_of("ipfabric")
-def hello_world(dispatcher, arg1=None):
-    """Run logic and return to user via client command '/ipfabric hello-world arg1'."""
-    if not arg1:
-        prompt_hello_input("ipfabric hello-world", "What would you like to say?", dispatcher)
-        return False
-
-    logger.debug("Received arg1 %s", arg1)
-    dispatcher.send_markdown(f"Just wanted to say {arg1}")
-    return True
-
-
-@subcommand_of("ipfabric")
-def device_list(dispatcher):
-    """IP Fabric Inventory device list."""
+def get_devices_info():
+    # TODO: move all the ipfabric into a class or/and implement some caching mechansm
     url = IPFABRIC_HOST + "/api/v1/tables/inventory/devices"
 
     logger.debug("Received device list request")
@@ -65,7 +52,91 @@ def device_list(dispatcher):
     headers = {"Accept": "application/json", "Content-Type": "application/json", "X-API-Token": API_TOKEN}
 
     response = requests.post(url, json=payload, headers=headers)
-    devices = response.json().get("data", {})
+    return response.json().get("data", {})
+
+
+def get_interfaces_load_info(device):
+    # TODO: move all the ipfabric into a class or/and implement some caching mechansm
+    url = IPFABRIC_HOST + "/api/v1/tables/interfaces/load"
+
+    logger.debug("Received device list request")
+
+    # columns and snapshot required
+    payload = {
+        "columns": ["intName", "inBytes", "outBytes"],
+        "filters": {"hostname": ["eq", device]},
+        "pagination": {"limit": 48, "start": 0},
+        "snapshot": "$last",
+        "sort": {"order": "desc", "column": "intName"},
+    }
+    # auth is contained in the 'X-API-Token' in the header
+    headers = {"Accept": "application/json", "Content-Type": "application/json", "X-API-Token": API_TOKEN}
+
+    response = requests.post(url, json=payload, headers=headers)
+    return response.json().get("data", {})
+
+
+def prompt_device_input(action_id, help_text, dispatcher, choices=None):
+    """Prompt the user for input."""
+    choices = [(device["hostname"], device["hostname"].lower()) for device in get_devices_info()]
+    dispatcher.prompt_from_menu(action_id, help_text, choices)
+    return False
+
+
+@subcommand_of("ipfabric")
+def get_int_load(dispatcher, device=None):
+    """Get interfaces load per device '/ipfabric get-int-load $device'."""
+    if not device:
+        prompt_device_input("ipfabric get-int-load", "Which device are you interested in", dispatcher)
+        return False
+
+    dispatcher.send_markdown(f"Load in interfaces for {device}.")
+    interfaces = get_interfaces_load_info(device)
+
+    dispatcher.send_blocks(
+        [
+            *dispatcher.command_response_header(
+                "ipfabric",
+                "get-int-load",
+                [],
+                "Interfaces Current Data",
+                ipfabric_logo(dispatcher),
+            ),
+            dispatcher.markdown_block(f"{IPFABRIC_HOST}/technology/interfaces/rate/inbound"),
+        ]
+    )
+
+    dispatcher.send_large_table(
+        ["IntName", "IN bps", "OUT bps"],
+        [
+            (
+                interface["intName"],
+                interface["inBytes"],
+                interface["outBytes"],
+            )
+            for interface in interfaces
+        ],
+    )
+
+    return True
+
+
+@subcommand_of("ipfabric")
+def hello_world(dispatcher, arg1=None):
+    """Run logic and return to user via client command '/ipfabric hello-world arg1'."""
+    if not arg1:
+        prompt_hello_input("ipfabric hello-world", "What would you like to say?", dispatcher)
+        return False
+
+    logger.debug("Received arg1 %s", arg1)
+    dispatcher.send_markdown(f"Just wanted to say {arg1}")
+    return True
+
+
+@subcommand_of("ipfabric")
+def device_list(dispatcher):
+    """IP Fabric Inventory device list."""
+    devices = get_devices_info()
 
     dispatcher.send_blocks(
         [
