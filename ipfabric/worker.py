fee376b48e47264bb4dbbc25dfd515d48c736f7d
pke11y <paddyk@gmail.com>
2021-08-13 13:42:40 +0100

    End-to-end path simulation (#17)
    * End-to-end path simulation

--- ipfabric/worker.py
+++ ipfabric/worker.py
@@ -4,7 +4,7 @@ import requests
 
 from django.conf import settings
 from django_rq import job
-
+from nautobot_chatops.choices import CommandStatusChoices
 from nautobot_chatops.workers import subcommand_of, handle_subcommands
 
 IPFABRIC_LOGO_PATH = "ipfabric/ipfabric_logo.png"
@@ -32,6 +32,84 @@ def prompt_hello_input(action_id, help_text, dispatcher, choices=None):
     return False
 
 
+def path_simulation(src_ip, dst_ip, src_port, dst_port, protocol, snapshot_id):  # pylint: disable=too-many-arguments
+    """Path Simulation from source to destination IP.
+
+    Args:
+        src_ip ([string]): Source IP
+        dst_ip ([string]): Destination IP
+        src_port ([string]): Source Port
+        dst_port ([string]): Destination Port
+        protocol ([string]): Transport Protocol
+        snapshot_id ([string]): Snapshot ID
+
+    Returns:
+        [list]: Parsed end-to-end path
+    """
+    response = ipfabric_api.get_path_simulation(src_ip, dst_ip, src_port, dst_port, protocol, snapshot_id)
+    graph = response.get("graph", {})
+    nodes = {graph_node["id"]: graph_node for graph_node in graph.get("nodes", {})}
+    edges = {edge["id"]: edge for edge in graph.get("edges", {})}
+    path = []
+
+    # ipfabric returns the source of the path as the last element in the nodes list
+    for idx, node in enumerate(graph.get("nodes", [])[::-1]):
+        edge_id = node["forwarding"][0]["dstIntList"][0]["id"]
+        edge = edges.get(edge_id)
+        if idx == len(nodes) - 1:
+            continue  # don't add to path as the edge for the penultimate node will contain the 'target' node
+        path.append(
+            (
+                idx + 1,
+                node.get("hostname"),
+                edge["slabel"],
+                edge["srcAddr"],
+                edge["dstAddr"],
+                edge["tlabel"],
+                nodes.get(edge["target"], {}).get("hostname"),
+            )
+        )
+    return path
+
+
+def get_src_dst_endpoint(
+    src_ip, dst_ip, src_port, dst_port, protocol, snapshot_id
+):  # pylint: disable=too-many-arguments
+    """Get the source/destination interface and source/destination node for the path.
+
+    Args:
+        src_ip ([string]): Source IP
+        dst_ip ([string]): Destination IP
+        src_port ([string]): Source Port
+        dst_port ([string]): Destination Port
+        protocol ([string]): Transport Protocol
+        snapshot_id ([string]): Snapshot ID
+
+    Returns:
+        [dict]: Src and Dst interface and node strings
+    """
+    response = ipfabric_api.get_path_simulation(src_ip, dst_ip, src_port, dst_port, protocol, snapshot_id)
+    graph = response.get("graph", {})
+
+    endpoints = {}
+    src_intf = ""
+    dst_intf = ""
+    src_node = ""
+    dst_node = ""
+
+    # ipfabric returns the source of the path as the last element in the nodes list
+    for idx, node in enumerate(graph.get("nodes", [])[::-1]):
+        if idx == 0:
+            src_intf = node["forwarding"][0]["srcIntList"][0]["int"]
+            src_node = node.get("hostname")
+            endpoints["src"] = f"{src_intf} - {src_node}"
+        if idx == len(graph.get("nodes", [])) - 1:
+            dst_intf = node["forwarding"][0]["dstIntList"][0]["int"]
+            dst_node = node.get("hostname")
+            endpoints["dst"] = f"{dst_intf} - {dst_node}"
+    return endpoints
+
+
 class IpFabric:
     """IpFabric will contain all the necessary API methods."""
 
@@ -42,8 +120,12 @@ class IpFabric:
 
     def get_response(self, url, payload):
         """Post request and return response dict."""
-        response = requests.post(self.host_url + url, json=payload, headers=self.headers)
-        return response.json().get("data", {})
+        return self.get_response_json("GET", url, payload).get("data", {})
+
+    def get_response_json(self, method, url, payload, params=None):
+        """GET request and return response dict."""
+        response = requests.request(method, self.host_url + url, json=payload, params=params, headers=self.headers)
+        return response.json()
 
     def get_devices_info(self):
         """Return Device info."""
@@ -72,6 +154,34 @@ class IpFabric:
         }
 
         return self.get_response("/api/v1/tables/interfaces/load", payload)
+
+    def get_snapshots(self):
+        """Return Snapshots."""
+        logger.debug("Received snapshot request")
+
+        # no payload required
+        payload = {}
+        return self.get_response_json("GET", "/api/v1/snapshots", payload)
+
+    def get_path_simulation(
+        self, src_ip, dst_ip, src_port, dst_port, protocol, snapshot_id
+    ):  # pylint: disable=too-many-arguments
+        """Return End to End Path Simulation."""
+        params = {
+            "source": src_ip,
+            "destination": dst_ip,
+            "sourcePort": src_port,
+            "destinationPort": dst_port,
+            "protocol": protocol,
+            "snapshot": snapshot_id,
+            # "asymmetric": asymmetric,
+            # "rpf": rpf,
+        }
+        logger.debug("Received end-to-end path simulation request: ", params)  # pylint: disable=logging-too-many-args
+
+        # no payload required
+        payload = {}
+        return self.get_response_json("GET", "/api/v1/graph/end-to-end-path", payload, params)
 
     def get_interfaces_errors_info(self, device):
         """Return bi-directional interface errors info."""
@@ -293,6 +403,81 @@ def device_list(dispatcher):
             for device in devices
         ],
     )
+    return True
+
+
+@subcommand_of("ipfabric")
+def end_to_end_path(
+    dispatcher, src_ip, dst_ip, src_port, dst_port, protocol, snapshot_id
+):  # pylint: disable=too-many-arguments, too-many-locals
+    """Execute end-to-end path simulation between source and target IP address."""
+    snapshots = [(snapshot.get("id", ""), snapshot.get("id", "")) for snapshot in ipfabric_api.get_snapshots()]
+
+    logger.info("Snapshots %s", snapshots)
+
+    dialog_list = [
+        {
+            "type": "text",
+            "label": "Source IP",
+        },
+        {
+            "type": "text",
+            "label": "Destination IP",
+        },
+        {
+            "type": "text",
+            "label": "Source Port",
+            "default": "1000",
+        },
+        {
+            "type": "text",
+            "label": "Destination Port",
+            "default": "22",
+        },
+        {
+            "type": "select",
+            "label": "Protocol",
+            "choices": [("TCP", "tcp"), ("UDP", "udp"), ("ICMP", "icmp")],
+            "default": ("TCP", "tcp"),
+        },
+        {
+            "type": "select",
+            "label": "Snapshot ID",
+            "choices": snapshots,
+            "default": snapshots[0],
+        },
+    ]
+
+    if not all([src_ip, dst_ip, src_port, dst_port, protocol, snapshot_id]):
+        dispatcher.multi_input_dialog("ipfabric", "end-to-end-path", "Path Simulation", dialog_list)
+        return CommandStatusChoices.STATUS_SUCCEEDED
+
+    dispatcher.send_blocks(
+        [
+            *dispatcher.command_response_header(
+                "ipfabric",
+                "end-to-end-path",
+                [],
+                "Path Simulation",
+                ipfabric_logo(dispatcher),
+            ),
+            dispatcher.markdown_block(f"{ipfabric_api.host_url}/graph/end-to-end-path"),
+        ]
+    )
+
+    # request simulation
+    path = path_simulation(src_ip, dst_ip, src_port, dst_port, protocol, snapshot_id)
+    endpoints = get_src_dst_endpoint(src_ip, dst_ip, src_port, dst_port, protocol, snapshot_id)
+
+    dispatcher.send_markdown(
+        f"{dispatcher.bold('Source: ')} {src_ip} [{endpoints.get('src')}]\n"
+        f"{dispatcher.bold('Destination: ')} {dst_ip} [{endpoints.get('dst')}]\n"
+    )
+    dispatcher.send_large_table(
+        ["Hop", "Src Host", "Src Intf", "Src IP", "Dst IP", "Dst Intf", "Dst Host"],
+        path,
+    )
+
     return True
 
 
