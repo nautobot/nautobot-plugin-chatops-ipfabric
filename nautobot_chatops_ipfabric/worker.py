914fcc49156118e7bb21eb74e45821965803402b
scetron <stephen.corry@networktocode.com>
2021-10-06 12:23:29 -0400

    run black

--- nautobot_chatops_ipfabric/worker.py
+++ nautobot_chatops_ipfabric/worker.py
@@ -636,7 +636,10 @@ def get_wireless_ssids(dispatcher, ssid=None, snapshot_id=None):
 
 def get_wireless_clients(dispatcher, ssid=None, snapshot_id=None):
     """Get Wireless Clients."""
-    ssids = [(f"{ssid_['wlanSsid']}-{ssid_['radioDscr']}", ssid_["wlanSsid"]) for ssid_ in ipfabric_api.get_wireless_ssids(snapshot_id)]
+    ssids = [
+        (f"{ssid_['wlanSsid']}-{ssid_['radioDscr']}", ssid_["wlanSsid"])
+        for ssid_ in ipfabric_api.get_wireless_ssids(snapshot_id)
+    ]
     if not ssids:
         dispatcher.send_blocks(
             [
